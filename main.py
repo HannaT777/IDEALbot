edfb25fd066ed0c879abff2f1ca23da329be01b3
HannaT777 <t0955809595@gmail.com>
2023-05-13 22:29:41 +0300

    hello phrase

--- main.py
+++ main.py
@@ -6,19 +6,7 @@ dispatcher = aiogram.Dispatcher(bot)
 
 @dispatcher.message_handler(commands=["start"])
 async def start(message: aiogram.types.Message):
-    await message.answer("Привіт!")
-    keyboard = aiogram.types.InlineKeyboardMarkup()
-    #Сюди треба додавати кнопки
-    keyboard.add(aiogram.types.InlineKeyboardButton("Перша кнопка", callback_data="first"))
-    keyboard.add(aiogram.types.InlineKeyboardButton("Друга кнопка", callback_data="second"))
-    await message.answer("Тестові кнопки", reply_markup=keyboard)
+    await message.answer("Привет! Я твой чат бот и помощник разобраться с собой. Работаю в подходе IDEAL. Напиши в поле ниже с какими мыслями ты пришел ко мне и что хотел бы получить в результате?")
 
-@dispatcher.callback_query_handler()
-async def callback(query: aiogram.types.CallbackQuery):
-    #Перевірка на натискання кнопки
-    if query.data == "first":
-        await bot.send_message(query.from_user.id, "Перша кнопка")
-    elif query.data == "second":
-        await bot.send_message(query.from_user.id, "Друга кнопка")
 
 aiogram.executor.start_polling(dispatcher)
